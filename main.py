339846e06c88672e6d3dda22e787e1c3df93989e
Rottie420 <93668355+Rottie420@users.noreply.github.com>
2023-11-17 09:52:26 +0000

    ****fix sitemap.xml, robots.txt, img_1 directories, and added new function for img_1

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
-def parking_page():
+def home_page():
     return render_template('home.html')
 
 @app.route('/robots.txt')
 def robots_txt():
-    return send_from_directory('static', 'robots.txt')
+    return send_from_directory(app.static_folder, 'robots.txt')
 
 @app.route('/sitemap.xml')
 def sitemap():
-    return send_from_directory('static', 'sitemap.xml')
+    return send_from_directory(app.static_folder, 'sitemap.xml')
 
+@app.route('/img_1.png')
+def sitemap_img():
+    return send_from_directory(app.static_folder, 'img_1.png')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
