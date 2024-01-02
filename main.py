83677e2beffca0ea37492a9fa5f5bd1ec1651827
Rottie420 <93668355+Rottie420@users.noreply.github.com>
2024-01-02 03:31:12 +0000

    ***added new contents for faqs

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, send_from_directory, request, redirect, jsonify
 import json
 
-
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
 
 @app.route('/')
 def landing_page():
   return render_template('main.html')
+
 
 @app.route('/homegrown')
 def homegrown_page():
@@ -22,11 +23,11 @@ def homegrown_page():
   content_3 = f_3.read()
   f_3.close()
 
-  return render_template('homegrown.html', 
+  return render_template('homegrown.html',
                          content_1=content_1,
                          content_2=content_2,
-                         content_3=content_3
-                        )
+                         content_3=content_3)
+
 
 @app.route('/blockchain')
 def blockchain_page():
@@ -46,41 +47,44 @@ def blockchain_page():
   content_4 = f_4.read()
   f_4.close()
 
-  
-  return render_template('blockchain.html', 
+  return render_template('blockchain.html',
                          content_1=content_1,
                          content_2=content_2,
                          content_3=content_3,
-                         content_4=content_4
-                        )
+                         content_4=content_4)
 
 
 @app.route('/aboutus')
 def aboutus_page():
   return render_template('aboutus.html')
-  
+
+
 @app.route('/robots.txt')
 def robots_txt():
-    return send_from_directory(app.static_folder, 'robots.txt')
+  return send_from_directory(app.static_folder, 'robots.txt')
+
 
 @app.route('/sitemap.xml')
 def sitemap():
-    return send_from_directory(app.static_folder, 'sitemap.xml')
+  return send_from_directory(app.static_folder, 'sitemap.xml')
+
 
 @app.route('/img_1.png')
 def sitemap_img():
-    return send_from_directory(app.static_folder, 'img_1.png')
+  return send_from_directory(app.static_folder, 'img_1.png')
+
 
 def get_subscribers():
   with open('subscribers.json', 'r') as file:
-      return json.load(file)
+    return json.load(file)
 
 
 def save_subscriber(email):
   subscribers = get_subscribers()
   subscribers.append(email)
   with open('subscribers.json', 'w') as file:
-      json.dump(subscribers, file, indent=2)
+    json.dump(subscribers, file, indent=2)
+
 
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
@@ -88,9 +92,11 @@ def subscribe():
   save_subscriber(email)
   return redirect('/')
 
+
 @app.route('/subscribers')
 def subscribers():
   return jsonify(get_subscribers())
+
 
 @app.route('/products')
 def products_page():
@@ -106,15 +112,21 @@ def products_page():
   content_3 = f_3.read()
   f_3.close()
 
-  f_4 = open('contents/homegrown_1.txt', encoding='utf-8', mode='r')
+  f_4 = open('contents/faq_4.txt', encoding='utf-8', mode='r')
   content_4 = f_4.read()
   f_4.close()
-  
-  return render_template('products.html', content_1=content_1, content_2=content_2, content_3=content_3, content_4=content_4)
 
+  f_5 = open('contents/faq_5.txt', encoding='utf-8', mode='r')
+  content_5 = f_5.read()
+  f_5.close()
 
-
+  return render_template('products.html',
+                         content_1=content_1,
+                         content_2=content_2,
+                         content_3=content_3,
+                         content_4=content_4,
+                         content_5=content_5)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
